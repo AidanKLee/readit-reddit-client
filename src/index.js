232484410fe52f26fa564d024c54f933133bacce
Aidan Lee <93386628+AidanKLee@users.noreply.github.com>
2021-12-22 00:27:51 +0000

    Changed filepaths in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './app/store';
+import store from './App/store';
 import { Provider } from 'react-redux';
 import './index.css';
 import './fonts.css';
-import App from './app/App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 
